a071967985c1ff54d1a500b57aa104b29295c8c7
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2020-04-24 23:13:16 -0500

    typo

--- main.tf
+++ main.tf
@@ -8,21 +8,10 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "UserId"
-  range_key      = "GameTitle"
 
   attribute {
     name = "UserId"
     type = "S"
-  }
-
-  attribute {
-    name = "Name"
-    type = "S"
-  }
-
-  attribute {
-    name = "Team"
-    type = "N"
   }
 
 }
